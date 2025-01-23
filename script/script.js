2b2d498777e6b7fe19a72bfc7ccae769df0778f1
Margarita-M5 <margarita.sevko@gmail.com>
2025-01-23 16:55:30 +0300

    task 10

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-// + Задание 1 + //
+//  Задание 1
 
 let a = 1;
 
@@ -10,7 +10,7 @@ for (let b = 35; b >= 8; b--) {
   console.log(b);
 }
 
-// + Задание 2 + //
+//  Задание 2
 
 let c = 89;
 
@@ -19,7 +19,7 @@ while (c >= 11) {
   c--;
 }
 
-// + Задание 3 + //
+//  Задание 3
 
 let sum = 0;
 
@@ -28,7 +28,7 @@ for (let d = 1; d <= 100; d++) {
   sum += d;
 }
 
-// + Задание 4 + //
+//  Задание 4
 
 let sum1 = 0;
 
@@ -40,14 +40,14 @@ for (let f = 1; f <= 5; f++) {
   console.log("Sum " + f + " = " + sum1);
 }
 
-// + Задание 5 + //
+//  Задание 5
 
 for (let m = 8; m <= 56; m++) {
   if (m % 2 == 0) continue;
   console.log(m);
 }
 
-// + Задание 6 + //
+// Задание 6
 
 for (let k = 2; k <= 10; k++) {
   for (let l = 1; l <= 10; l++) {
@@ -55,7 +55,7 @@ for (let k = 2; k <= 10; k++) {
   }
 }
 
-// + Задание 7 + //
+//  Задание 7
 
 let n = 1000;
 let num = 0;
@@ -69,7 +69,7 @@ while (n >= 50) {
 n--;
 console.log(num);
 
-// + Задание 8 + //
+//  Задание 8
 
 let numb = prompt("Введите число", 0),
   p = 0,
@@ -90,18 +90,48 @@ document.write(
     "."
 );
 
-// + Задание 9 + //
+//  Задание 9
 
-let str = "4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57",
-min = Infinity,
-max = -Infinity,
-buffer = " ";
+let str1 = "4 98 4 6 1 32 4 65 4 3 5 7 89 7 10 1 36 8 57",
+  min = Infinity,
+  max = -Infinity,
+  buffer = " ";
 
-for (i = 0; i < str.length; i++) {
-  if (str[i] === " ") {
-     buffer += str[i];
-  } else{
-    if (min > +buffer) min = +buffer;
-    if (max < +buffer) max = +buffer;
-    buffer = '';
-};
+for (let i = 0; i < str1.length; i++) {
+  if (str1[i] === " ") {
+    if (Number(buffer) > max) max = Number(buffer);
+    if (Number(buffer) < min) min = Number(buffer);
+    buffer = " ";
+  } else {
+    buffer = buffer + str1[i];
+  }
+}
+console.log("Минимальное число: " + min + " Максимальное число: " + max);
+
+//  Задание 10
+
+let numString = String(n),
+  numCount = numString.length,
+  summ = 0,
+  s = 0,
+  numRev = " ";
+
+for (let i = 0; i < numString.length; i++) {
+  console.log(+numString[i]);
+  summ += +numString[i];
+}
+
+for (let i = numString.length; i > 0; i++) {
+  numRev += numString[i];
+}
+
+console.log(
+  "Исходное значение: " +
+    s +
+    " Количество цифр: " +
+    numCount +
+    " Сумма цифр: " +
+    summ +
+    " Обратный порядок цифр: " +
+    numRev
+);
